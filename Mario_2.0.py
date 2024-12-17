8e80390e8896673ee44105ecc8ba339d2a479283
Mithunn3011 <mithunn3011@gmail.com>
2024-12-17 22:24:29 +0100

    Add files via upload

---
+++ Mario_2.0.py
@@ -0,0 +1,100 @@
+'''
+Programme de base de Mario.
+Il n'utilise pas les classes de python, ce qui compliquera l'écriture du
+programme avec l'ajout de personnages.
+'''
+import pygame
+from pygame.locals import *
+from B_sprites import *
+from Mario_Class_Final import *
+pygame.init()
+
+index = 0
+xp = 50
+yp = 296
+deplt = 10
+sens = "droite"
+
+continuer = True
+'''
+Boucle principale (évenement) du jeu. Elle s'exécute tant que Quit
+n'est pas cliqué (dans ce cas la variable continuer = False)
+'''
+mario=Mario(mario_sprite,xp,yp,deplt)
+gomma=Personnage(gooma_sprite,600,296,deplt-5)
+eagle1=Personnage(eagle_sprite,0,30,deplt+5)
+eagle2=Personnage(eagle_sprite,-200,30,deplt+2)
+while continuer:
+    #détection des événements de type clavier notamment
+    for event in pygame.event.get():
+        if event.type == QUIT :
+            continuer = False
+        if event.type == KEYDOWN :
+            if event.key == K_RIGHT :
+                sens = "droite"
+                image_mario=mario.deplacer_droite()
+            elif event.key == K_LEFT :
+                sens = "gauche"
+                image_mario=mario.deplacer_gauche()
+            elif event.key == K_SPACE:  # Détection de la touche '2'
+                image_mario = mario.saute()
+        else:       # Mario a l'arrêt
+            if mario.sens == "droite" :
+                image_mario=mario.stop()
+            else:
+                image_mario=mario.stop()
+    if mario.xp-gomma.xp>0:
+        image_gomma=gomma.deplacer_droite()
+    elif mario.xp-gomma.xp<0:
+        image_gomma=gomma.deplacer_gauche()
+    if eagle1.xp<=650:
+        image_eagle1=eagle1.deplacer_droite()
+    else:
+        eagle1.xp=0
+    if eagle2.xp<=650:
+        image_eagle2=eagle2.deplacer_droite()
+    else:
+        eagle2.xp=0
+    # Gestion du saut de Mario
+    if mario.is_jumping:
+        # Déplacement vertical
+        mario.yp += mario.jump_velocity  # Déplace Mario verticalement
+        mario.jump_velocity += 2  # Simule la gravité
+
+        # Pendant le saut, maintenir l'image de saut
+        if mario.sens == "droite":
+            image_mario = mario.sprites[16]  # Sprite de saut à droite
+        else:
+            image_mario = mario.sprites[17]  # Sprite de saut à gauche
+
+        # Fin du saut (Mario touche le sol)
+        if mario.yp >= 296:
+            mario.yp = 296  # Mario revient au sol
+            mario.is_jumping = False  # Le saut est terminé
+            mario.jump_velocity = 0
+            image_mario = mario.stop()  # Retour à l'image de repos
+        elif mario.yp <= 130 and mario.yp >= 120 and mario.xp>=315 and mario.xp<=525 :
+            mario.yp = 130  # Mario revient au sol
+            mario.jump_velocity = 0
+            image_mario = mario.stop()
+        elif mario.yp <= 130 and mario.yp >= 120 and mario.xp>=150 and mario.xp<=190 :
+            mario.yp = 130  # Mario revient au sol
+            mario.jump_velocity = 0
+            image_mario = mario.stop()
+        elif mario.yp <= 130 and mario.yp >= 120 :
+            mario.yp += mario.jump_velocity  # Déplace Mario verticalement
+
+
+
+    pygame.time.Clock().tick(10)
+    #réaffichage de la fenêtre
+    fenetre.blit(fond,(0,0))
+    fenetre.blit(image_mario,(mario.xp,mario.yp))
+    fenetre.blit(image_gomma,(gomma.xp,gomma.yp))
+    fenetre.blit(image_eagle1,(eagle1.xp,eagle1.yp))
+    fenetre.blit(image_eagle2,(eagle2.xp,eagle2.yp))
+
+    pygame.display.flip()
+
+#Fermeture de  pygame
+pygame.quit()
---
+++ Mario_Class_Final.py
@@ -0,0 +1,46 @@
+
+class Personnage:
+    def __init__(self,sprites,xp,yp,deplt):
+        self.sprites=sprites
+        self.xp=xp
+        self.yp=yp
+        self.deplt=deplt
+        self.index=0
+        self.sens='droite'
+        self.is_jumping = False  # Nouveau : Mario saute ou non
+        self.jump_time = 0
+
+    def deplacer_droite(self):
+        self.sens='droite'
+        self.xp +=self.deplt
+        self.index = (self.index + 1) % 6
+        return self.sprites[self.index]
+    def deplacer_gauche(self):
+        self.sens='gauche'
+        self.xp -=self.deplt
+        self.index = ((self.index + 1) % 6)+6
+        return self.sprites[self.index]
+    def stop(self):
+        if self.sens=='droite':
+            return self.sprites[12]
+        elif self.sens=='gauche':
+            return self.sprites[13]
+
+class Mario(Personnage):	# la classe Mario hérite de la classe Personnage
+    # la déclaration du constructeur de Mario est identique
+    # à celle du constructeur de Personnage
+    def __init__(self, sprite, xp = 50, yp = 296, deplt = 10):
+        # Appel explicite au constructeur de la classe Personnage
+        Personnage.__init__(self, sprite, xp = 50, yp = 296, deplt = 10)
+        # D’autres propriétés peuvent être rajoutées ici
+        self.gravite=9.8
+
+    def saute(self):
+        if not self.is_jumping:
+            self.is_jumping = True
+            self.jump_velocity = -25  # Vitesse initiale pour monter
+            if self.sens == 'droite':
+                return self.sprites[16]
+            else:
+                return self.sprites[17]
+
