f0d510e4ce2ca3c9cb8bc04a5166302f7987bd25
Mithunn3011 <mithunn3011@gmail.com>
2024-12-19 22:58:05 +0100

    Update C_mario_classe.py

--- C_mario_classe.py
+++ C_mario_classe.py
@@ -9,20 +9,22 @@ from B_sprites import *
 from C_classes import *
 pygame.init()
 
+#Variables
 index = 0
 xp = 50
 yp = 296
 deplt = 10
-
 continuer = True
 '''
 Boucle principale (évenement) du jeu. Elle s'exécute tant que Quit
 n'est pas cliqué (dans ce cas la variable continuer = False)
 '''
+# Les Personnages
 mario=Personnage(mario_sprite,xp,yp,deplt)
 gomma=Personnage(gooma_sprite,600,296,deplt-5)
 eagle1=Personnage(eagle_sprite,0,30,deplt+5)
 eagle2=Personnage(eagle_sprite,-200,30,deplt+2)
+
 while continuer:
     #détection des événements de type clavier notamment
     for event in pygame.event.get():
@@ -34,16 +36,20 @@ while continuer:
             if event.key == K_LEFT :
                 image_mario=mario.deplacer_gauche()
             if event.key== K_UP :
-                yp = yp
-        else:       # Mario a l'arrêt
+                mario.yp -= 10
+            if event.key== K_DOWN :
+                mario.yp += 10
+        else:# Si Mario s'arrête
             if mario.sens == "droite" :
                 image_mario=mario.stop()
             else:
                 image_mario=mario.stop()
+     #Le déplacement de gomma
     if mario.xp-gomma.xp>0:
         image_gomma=gomma.deplacer_droite()
     elif mario.xp-gomma.xp<0:
         image_gomma=gomma.deplacer_gauche()
+    #Le déplacement des eagles
     if eagle1.xp<=650:
         image_eagle1=eagle1.deplacer_droite()
     else:
@@ -52,8 +58,10 @@ while continuer:
         image_eagle2=eagle2.deplacer_droite()
     else:
         eagle2.xp=0
+
+
     pygame.time.Clock().tick(10)
-    #réaffichage de la fenêtre
+    #l'Affichage de la fenêtre
     fenetre.blit(fond,(0,0))
     fenetre.blit(image_mario,(mario.xp,mario.yp))
     fenetre.blit(image_gomma,(gomma.xp,gomma.yp))
