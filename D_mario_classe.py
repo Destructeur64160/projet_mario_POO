1918f4ac8c01517930acd385c009576bc6dd0439
Mithunn3011 <mithunn3011@gmail.com>
2024-12-19 22:47:40 +0100

    Update D_mario_classe.py

--- D_mario_classe.py
+++ D_mario_classe.py
@@ -9,22 +9,26 @@ from B_sprites import *
 from D_classes import *
 pygame.init()
 
+#Variables
 index = 0
 xp = 50
 yp = 296
 deplt = 10
-sens_eagle="droite"
-
 continuer = True
 '''
 Boucle principale (évenement) du jeu. Elle s'exécute tant que Quit
 n'est pas cliqué (dans ce cas la variable continuer = False)
 '''
+#Les Personnages
 mario=Mario(mario_sprite,xp,yp,deplt)
 gomma=Personnage(gooma_sprite,600,296,deplt-5)
 eagle1=Personnage(eagle_sprite,0,30,deplt+5)
 eagle2=Personnage(eagle_sprite,-200,30,deplt+2)
+
 while continuer:
+    # Changer les rectangles en fonction des coordonées des personnages
+    mario.rectangle.topleft = (mario.xp, mario.yp)
+    gomma.rectangle.topleft = (gomma.xp, gomma.yp)
     #détection des événements de type clavier notamment
     for event in pygame.event.get():
         if event.type == QUIT :
@@ -34,17 +38,20 @@ while continuer:
                 image_mario=mario.deplacer_droite()
             elif event.key == K_LEFT :
                 image_mario=mario.deplacer_gauche()
-            elif event.key == K_SPACE:  # Détection de la touche '2'
+            elif event.key == K_SPACE:
                 image_mario = mario.saute()
-        else:       # Mario a l'arrêt
+
+        else:#Si mario s'arrête
             if mario.sens == "droite" :
                 image_mario=mario.stop()
             else:
                 image_mario=mario.stop()
+    #Déplacement du gomma
     if mario.xp-gomma.xp>0:
         image_gomma=gomma.deplacer_droite()
     elif mario.xp-gomma.xp<0:
         image_gomma=gomma.deplacer_gauche()
+     #Déplacement des eagles
     if eagle1.xp<=650:
         image_eagle1=eagle1.deplacer_droite()
     else:
@@ -53,6 +60,8 @@ while continuer:
         image_eagle2=eagle2.deplacer_droite()
     else:
         eagle2.xp=0
+
+
     # Gestion du saut de Mario
     if mario.is_jumping:
         # Déplacement vertical
@@ -72,23 +81,16 @@ while continuer:
             mario.jump_velocity = 0
             image_mario = mario.stop()  # Retour à l'image de repos
 
-    if mario.rectangle.colliderect(gomma.rectangle):
-        fenetre.blit(mort,(0,0))
-        print("Game Over !")
-        #continuer = False
-    # elif mario.rectangle.colliderect(gomma.rectangle) == False :
-    #     print("Oh !")
+    if mario.rectangle.colliderect(gomma.rectangle):# Vérification des collisions entre mario et gomma
+        fenetre.blit(mort,(0,0))#Affichel'image de fin
     else:
         pygame.time.Clock().tick(10)
-        #réaffichage de la fenêtre
+        #l'Affichage de la fenêtre
         fenetre.blit(fond,(0,0))
         fenetre.blit(image_mario,(mario.xp,mario.yp))
         fenetre.blit(image_gomma,(gomma.xp,gomma.yp))
         fenetre.blit(image_eagle1,(eagle1.xp,eagle1.yp))
         fenetre.blit(image_eagle2,(eagle2.xp,eagle2.yp))
-        pygame.draw.rect(fenetre, (255, 0, 0), mario.rectangle, 2)  # Rectangle de Mario en rouge
-        pygame.draw.rect(fenetre, (0, 255, 0), gomma.rectangle, 2)  # Rectangle de Gomma en vert
-
     pygame.display.flip()
 
 #Fermeture de  pygame
